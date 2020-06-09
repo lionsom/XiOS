01b5079e7f432a688e6e83b24a7e9db22254ac9e
lionsom <597207909@qq.com>
2020-06-09 09:57:37 +0800

    swift

--- iOS/Swift/LXSwift/LXSwift/Root/FirstViewController.swift
+++ iOS/Swift/LXSwift/LXSwift/Root/FirstViewController.swift
@@ -18,6 +18,10 @@ class FirstViewController: UIViewController {
         
         self.title = "第一页";
         
+        // 自定义 Appearance
+        let navigationBarAppearance = self.navigationController!.navigationBar
+        navigationBarAppearance.backgroundColor = UIColor.randomColor()
+        
         // 导航栏风格
         self.navigationController!.navigationBar.barStyle = .black
         // 导航栏是否半透明
@@ -30,14 +34,43 @@ class FirstViewController: UIViewController {
         // 导航栏titile颜色
         self.navigationController!.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.orange]
 
+        // 导航栏左侧按钮
+        let leftBarButtonItem1 = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(barButtonItemClick(sender:)))
+        leftBarButtonItem1.tag = 100;
+        let leftBarButtonItem2 = UIBarButtonItem(title: "相册", style: .plain, target: self, action: nil)
+        leftBarButtonItem2.action = #selector(barButtonItemClick(sender:))
+        leftBarButtonItem2.tag = 101;
+        self.navigationItem.leftBarButtonItems = [leftBarButtonItem1, leftBarButtonItem2]
         
         // 自定义导航栏右侧视图
         let myUIView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
         myUIView.backgroundColor = UIColor.purple
+        // view tap
+        let singleTapGesture = UITapGestureRecognizer(target: self, action: #selector(tapClick(tap:)))
+        myUIView.addGestureRecognizer(singleTapGesture)
+        
         let rightButton = UIBarButtonItem(customView: myUIView)
         // 加到導覽列中
         self.navigationItem.rightBarButtonItem = rightButton
 
+        // 自定义导航栏Title View
+        let titleV = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+        titleV.backgroundColor = UIColor.red
+        self.navigationItem.titleView = titleV
+        
+        // 返回按钮 移除文字
+        let backBarButtton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backBarButtton
+        
+        // 导航栏变大
+        if #available(iOS 11.0, *) {
+//            self.navigationController?.navigationBar.prefersLargeTitles = true
+//            self.title = "我是大标题"
+        } else {
+            // Fallback on earlier versions
+        }
+
+        
         // 建立一個按鈕
         let myButton = UIButton(frame: CGRect(x: 100, y: 250, width: 120, height: 40))
 //        myButton.setTitle("回前頁", forState:UIButton.State.normal)
@@ -48,8 +81,31 @@ class FirstViewController: UIViewController {
         self.view.addSubview(myButton)
     }
     
-    
+   
     // MARK: - ====== Actions ======
+        
+    @objc func barButtonItemClick(sender:UIBarButtonItem) {
+        if sender.tag == 100 {
+            Log("barButtonItemClick 1 Click")
+            
+            navigationItem.prompt = NSLocalizedString("Navigation prompts appear at the top.", comment: "啊水电费")
+        }
+        if sender.tag == 101 {
+            Log("barButtonItemClick 2 Click")
+            
+            navigationItem.prompt = nil
+        }
+    }
+    
+    @objc func tapClick(tap:UITapGestureRecognizer) {
+        Log("tap Click")
+        
+        let AlgorithmVC = AlgorithmTableViewController()
+        self.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(AlgorithmVC, animated: true)
+        self.hidesBottomBarWhenPushed = false
+    }
+    
     @objc func back() {
         Log("点击了")
         
