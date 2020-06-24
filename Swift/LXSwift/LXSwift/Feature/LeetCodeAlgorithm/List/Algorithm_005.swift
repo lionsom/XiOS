3a53e65990570b5acb80aeb104e2c23c4bbc72de
lionsom <597207909@qq.com>
2020-06-24 10:32:46 +0800

    Leecode

--- Swift/LXSwift/LXSwift/Feature/LeetCodeAlgorithm/List/Algorithm_005.swift
+++ Swift/LXSwift/LXSwift/Feature/LeetCodeAlgorithm/List/Algorithm_005.swift
@@ -17,31 +17,46 @@ class Algorithm_005: UIViewController {
         self.view.backgroundColor = UIColor.randomColor()
         self.title = "最长公共前缀";
         
-        let Arr = ["aasdad", "asdf", "aldsdf", "aasdadfs"];
+        let Arr = ["aasdad", "aasdf", "aaldsdf", "aasdadfs"];
         
-        let preStr = getSomePre(arr: Arr)
+        let preStr = longestCommonPrefix(Arr)
         Log("最长公共前缀 :" + preStr)
     }
     
-    func getSomePre(arr: [String]) -> String {
-        // 先找到最长的字符串
-        var longStr: String? = arr.first
-        for str in arr {
-            if str.count > longStr!.count {
-                longStr = str
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        // 1.先找到最短的字符串
+        var shortStr: String? = strs.first
+        
+        if shortStr == nil {
+            return ""
+        }
+        
+        for str in strs {
+            if str.count < shortStr!.count {
+                shortStr = str
             }
         }
         
-        // 从最大字符串开始匹配，然后一次移除最后一个字符
-        for str in arr {
-            if !str.hasPrefix(longStr!) {
-                longStr!.remove(at: longStr!.endIndex)
-                continue
+        // 递归函数
+        func Recursion(str: String) -> String {
+            // 标记是否跳出for循环
+            var isBreak = false
+            for tempStr in strs {
+                // 遇到一个不匹配的就跳出循环
+                if !tempStr.hasPrefix(str) {
+                    isBreak = true
+                    break
+                }
             }
+            // 如果被中断了，则移除末尾最后一个字符，再次循环
+            if isBreak {
+                var newStr: String = str   // let -> var
+                newStr.removeLast()
+                return Recursion(str: newStr)
+            }
+            return str
         }
-        
-        
-        return "af"
+        return Recursion(str: shortStr!)
     }
     
     
