7388cbb7d606fcdc21ec28f4e9a52c08db176b0d
lionsom <597207909@qq.com>
2020-06-28 21:20:06 +0800

    leetcode

--- Swift/LXSwift/LXSwift/Feature/LeetCodeAlgorithm/Algorithm_006.swift
+++ Swift/LXSwift/LXSwift/Feature/LeetCodeAlgorithm/Algorithm_006.swift
@@ -18,9 +18,44 @@ class Algorithm_006: UIViewController {
         self.title = "有效的括号";
         
         // 包括 () [] {}
-        let str = "([{()}])[({[]})]"
-        Log("\(str) 是否为有效括号？结果：\(isValid(str))")
+        let str = "([{()}])[({[]})][([)]]"
+//        Log("\(str) 是否为有效括号？结果：\(isValid(str))")
+        
+        Log("\(str) 是否为有效括号？结果：\(method_A(str))")
     }
+    
+    /*
+     执行用时：976 ms,在所有Swift提交中击败了 5.16% 的用户
+     内存消耗：21.7 MB, 在所有 Swift 提交中击败了33.33%的用户
+     */
+    func method_A(_ str: String) -> Bool {
+          // 对其进行特殊情况判断
+          // 1.第一位为右侧符号，直接判假；最后一位为左侧符号，直接判假
+          // 2.字符串个数为奇数，直接判假
+          if str.count % 2 != 0 ||
+              str.first == ")" || str.first == "]" ||  str.first == "}" ||
+              str.last == "(" || str.last == "[" || str.last == "{" {
+              return false
+          }
+          
+          var S = str
+          while true {
+              let temp = S
+              
+              S = S.replacingOccurrences(of: "()", with: "")
+              S = S.replacingOccurrences(of: "[]", with: "")
+              S = S.replacingOccurrences(of: "{}", with: "")
+              
+              if temp == S {
+                  break
+              }
+          }
+          if S.count == 0 {
+              return true
+          }
+          return false
+      }
+    
     
     fileprivate var charDic: [Character: Character] = ["(":")", "[":"]", "{":"}"]
     
@@ -69,20 +104,6 @@ class Algorithm_006: UIViewController {
     }
     
     
-    
-//    fileprivate struct Char {
-//        struct typeA {
-//            let left: Character = "("
-//            let right: Character = ")"
-//        }
-//        struct typeB {
-//            let left: Character = "["
-//            let right: Character = "]"
-//        }
-//        struct typeC {
-//            let left: Character = "{"
-//            let right: Character = "}"
-//        }
-//    }
+  
     
 }
